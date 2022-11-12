479a5f989d25af00b4829b797f63162e40838b2f
Jackson Gunther <jackson.gunther@yahoo.com>
2022-11-12 11:58:04 -0700

    updates

--- static/javascript/index.js
+++ static/javascript/index.js
@@ -1,5 +1,8 @@
 var currentTab = 0; // Current tab is set to be the first tab (0)
-showTab(currentTab); // Display the current tab
+// let tabElement = $('.tab'[0]);
+if(window.location.href.match("/support")) {
+  showTab(currentTab); // Display the current tab
+}
 
 function showTab(n) {
   // This function will display the specified tab of the form ...
