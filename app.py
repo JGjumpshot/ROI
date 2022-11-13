2785fd3c3dc06a6c8595eaadce5ec963768815b6
Jackson Gunther <jackson.gunther@yahoo.com>
2022-11-13 12:35:41 -0700

    refactored lists of checkboxes to be less code and more repeatable

--- app.py
+++ app.py
@@ -7,6 +7,11 @@ app = Flask(__name__)
 
 EMAIL = os.getenv('EMAIL')
 PASSWORD = os.getenv('PASSWORD')
+contact_types = ['Chat', 'Email', 'Phone', 'Other']
+languages_tab_1 = ['English','French', 'Spanish', 'Portuguese', 'Polish', 'Turkish', 'Czech']
+languages_tab_2 = ['Dutch', 'Norwegian', 'Swedish', 'Finnish', 'German', 'Danish', 'French Canadian']
+languages_tab_3 = ['Russian', 'Arabic', 'Hindi', 'Romanian', 'Hungarian', 'Mandarin', 'Greek Italian']
+industries = ['Technology', 'Healthcare', 'Retail / E-commerce', 'Financial Education', 'Education', 'Business', 'Real Estate', 'Automotive', 'Government']
 
 @app.route('/')
 def home():
@@ -16,7 +21,7 @@ def home():
 @app.route('/support')
 def support():
     title="Get Started"
-    return render_template("support.html", title=title)
+    return render_template("support.html", title=title, len = len(contact_types), contact_types=contact_types, lenl1=len(languages_tab_1), languages_tab_1=languages_tab_1, lenl2=len(languages_tab_2), languages_tab_2=languages_tab_2, lenl3= len(languages_tab_3), languages_tab_3=languages_tab_3, leni=len(industries), industries=industries)
 
 @app.route('/submit', methods=['POST'])
 def submit():
